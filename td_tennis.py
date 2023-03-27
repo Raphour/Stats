a7c4de27e8f7e5761735631c257d6b9cf9544009
Raphael SILLY <E226767L@univ-nantes.fr>
2023-03-27 14:48:50 +0200

    bien le sang tia mis le kimono et tout?

--- td_tennis.py
+++ td_tennis.py
@@ -32,34 +32,41 @@ def tailleJoueurs(anneeNaissance):
 
 
 #Ex2
-def evolutionRang(identifiant):
-
-
-    rechercheJoueurIdentifiant = donneesJoueurs[donneesJoueurs["player_id"]==identifiant]
-
-    rankingJoueur = rankingFinal[rankingFinal["player_id"]==identifiant]["rank_number"]
-    
-    dateRank = rankingFinal[rankingFinal["player_id"]==identifiant]["week_title"]
-    dateRankNumpy = np.array([date for date in dateRank])
-    dates = np.array([datetime.datetime(int(date[0:4]),int(date[5:7]),int(date[8:11])) for date in dateRankNumpy])
-
-    ordreDates = np.argsort(dates)
-
-    datesRangees = dateRankNumpy[ordreDates]
-
- 
-
-
-
-    plt.plot(datesRangees, rankingJoueur)
-    plt.ylim(max(rankingJoueur)+5,min(rankingJoueur)-5)
-
-    plt.locator_params(axis='x', nbins=3)
-    
+def evolutionRang(id):
+    player = rankingFinal[rankingFinal['player_id']==id]
+    player['week_title'] = pd.to_datetime(player['week_title'], format='%Y.%m.%d')
+    player = player.sort_values(by='week_title')
+    #Inverser l'axe y
+    title = "Evolution du classement de "+donneesJoueurs[donneesJoueurs['player_id']==id]['first_name'].values[0]+" "+donneesJoueurs[donneesJoueurs['player_id']==id]['last_name'].values[0]
+    evolution = player.plot(x='week_title',y='rank_number',title=title,figsize=(15, 10), fontsize=12,legend=False)
+    plt.gca().invert_yaxis()
+    evolution.set_xlabel("", fontsize=12)
+    evolution.set_ylabel("Classement", fontsize=12)
     plt.show()
 
 
 
 #Ex 3
 def lienTaillePoints():
-    
+    joueurheight = donneesJoueurs[(donneesJoueurs['height_cm']>6.5)][['player_id','height_cm']]
+    ServReussis = donneesMatchStats[(donneesMatchStats['winner_first_serve_points_won']<=donneesMatchStats['winner_first_serves_in'])&(donneesMatchStats['loser_first_serve_points_won']<=donneesMatchStats['loser_first_serves_in'])&(donneesMatchStats['winner_first_serves_in']>0)&(donneesMatchStats['loser_first_serves_in']>0)][['match_id','winner_first_serve_points_won','loser_first_serve_points_won','winner_first_serves_in','loser_first_serves_in']]
+    winners = donneesScores.merge(ServReussis, on='match_id', how='inner')
+    losers = donneesScores.merge(ServReussis, on='match_id', how='inner')
+    winners = winners.merge(joueurheight, left_on='winner_player_id', right_on='player_id', how='inner')
+    losers = losers.merge(joueurheight, left_on='loser_player_id', right_on='player_id', how='inner')
+    winnerheight = winners[['player_id','height_cm','winner_first_serve_points_won','winner_first_serves_in']]
+    loserheight = losers[['player_id','height_cm','loser_first_serve_points_won','loser_first_serves_in']]
+    general = pd.concat([winnerheight,loserheight])
+    general = general.groupby('player_id').mean()
+    general['first_serve_points_won'] = (general['winner_first_serve_points_won']+general['loser_first_serve_points_won'])/(general['winner_first_serves_in']+general['loser_first_serves_in'])
+    #Enlever les NaN
+    general = general.dropna()
+    print(general)
+    plt.scatter(general['height_cm'],general['first_serve_points_won'],s=1)
+    plt.title("Nuage de points des joueurs en fonction de leur taille et du nombre de points gagnés sur leur premier service")
+    plt.xlabel("Taille en cm")
+    plt.ylabel("Nombre de points gagnés sur le premier service")
+    plt.yticks = np.arange(0.1,0.9,0.1)
+    plt.show()
+
+lienTaillePoints()
